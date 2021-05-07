18f83854bdda220e1ca9cb7a9ea0ca6c085cd66e
ShatoKid008 <83462531+ShatoKid008@users.noreply.github.com>
2021-05-07 09:52:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# import math
+
+# Finding square root of a number with user input (INT only)
+
+# sq = input("Enter the number want the square root of: ")
+#
+# ans = math.sqrt(int(sq))
+#
+# print(ans)
+
+# Greatest common denominator with user input (INT only)
+
+# input1 = input("please enter the first number :")
+# input2 = input("please enter the second number :")
+# gcd2 = math.gcd(int(input1), int(input2))
+#
+# print(gcd2)
+
+# Finding the power of a number with and without user input (INT only)
+
+# print(math.pow(100, 2))
+
+# BS = input("enter a base: ")
+# PW = input("enter an exponent: ")
+# BSPW = math.pow(int(BS), int(PW))
+#
+# print(BSPW)
+
+# Displaying Fibonnaci
+# import fibo
+#
+# fibo.fib(1000)
